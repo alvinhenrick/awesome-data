c9d28dd6adf3084a8411bc94c8311123fabb20d9
alvin <share.code@aol.com>
2016-05-12 20:41:47 -0500

    move to project

--- enter-hadoop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#docker run -it --rm --env-file=./hadoop.env --net hadoop awesomedata/hadoop hadoop fs -mkdir -p /user/root
-
-ID=$1
-docker run -it --rm --env-file=./hadoop.env --net hadoop --name temp-hadoop-${ID} awesomedata/hadoop bash
--- hue/entrypoint.sh
+++ hue/entrypoint.sh
@@ -3,7 +3,7 @@
 HUE_HOME="/opt/hue/"
 HUE_CONFIG_FILE="$HUE_HOME/desktop/conf/pseudo-distributed.ini"
 APP_BLACKLIST="search,security,oozie,jobbrowser,pig,beeswax,search,zookeeper,impala,rdbms,spark,metastore,hbase,sqoop,jobsub,about"
-NAMENODE_HOST="namenode"
+NAMENODE_HOST="namenode.hadoop"
 
 sed -i "s#.*blacklist.*#  app_blacklist=$APP_BLACKLIST#g" $HUE_CONFIG_FILE
 sed -i "s#.*webhdfs_url.*#  webhdfs_url=http://$NAMENODE_HOST:50070/webhdfs/v1#g" $HUE_CONFIG_FILE
