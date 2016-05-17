23ab1866d6f40dd3b8cdfd4ef9ec7ef4b41350e1
alvin <alvin@olemedia.com>
2016-05-16 20:25:36 -0500

    remove hue

--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for i in baseimage hadoop namenode datanode resourcemanager nodemanager historyserver spark spark-master spark-worker spark-notebook zookeeper kafka hue; do
+for i in baseimage hadoop namenode datanode resourcemanager nodemanager historyserver spark spark-master spark-worker spark-notebook zookeeper kafka; do
     ( cd $i && ./build.sh)
 done
